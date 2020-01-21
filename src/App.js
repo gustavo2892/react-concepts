050ffeb9a82781ee0b976e83bbd676b9ca0b4a2c
gustavo2892 <gustavo2892@gmail.com>
2020-01-21 00:37:27 -0300

    Finalized basic base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+
 import './App.css';
+import profile from './assets/react.png';
+
+import TechList from './components/TechList';
 
 function App() {
-  return <h1>Hello Rocketseat</h1>
+  return (
+    <>
+      <h1>Hello Rocketseat</h1>
+      <img width="200" src={profile} />
+      <TechList/>
+    </>
+  );
 }
 
 export default App;
